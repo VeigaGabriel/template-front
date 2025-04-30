1832691ee0663570709388e38389290470c516b6
VeigaGabriel <gabrielvalmeidaa@gmail.com>
2025-04-30 12:37:34 -0300

    chore: remove sidebar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,48 +26,44 @@ export default function Header() {
 
   return (
     <header>
-      <SidebarProvider>
-        <Sidebar />
-        <div>
-          <div
-            className={cn(['flex', 'h-18', 'items-center', 'justify-end', 'gap-2', 'p-4'])}
-            style={{ backgroundColor: theme.card }}
-          >
-            <Avatar className="w-12 h-12">
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+      <div>
+        <div
+          className={cn(['flex', 'h-18', 'items-center', 'justify-end', 'gap-2', 'p-4'])}
+          style={{ backgroundColor: theme.card }}
+        >
+          <Avatar className="w-12 h-12">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
 
-            {routesList.map(({ link, name }, index) => (
-              <div
-                className="flex flex-row h-full items-center gap-2"
-                key={link}
-              >
-                {index === 0 && (
-                  <Separator
-                    className="my-4"
-                    orientation="vertical"
-                    style={{ backgroundColor: theme.primary }}
-                  />
-                )}
-                <Link
-                  className="font-bold text-md"
-                  to={link}
-                  style={{ color: theme.primary }}
-                >
-                  {name}
-                </Link>
+          {routesList.map(({ link, name }, index) => (
+            <div
+              className="flex flex-row h-full items-center gap-2"
+              key={link}
+            >
+              {index === 0 && (
                 <Separator
                   className="my-4"
                   orientation="vertical"
                   style={{ backgroundColor: theme.primary }}
                 />
-              </div>
-            ))}
-          </div>
+              )}
+              <Link
+                className="font-bold text-md"
+                to={link}
+                style={{ color: theme.primary }}
+              >
+                {name}
+              </Link>
+              <Separator
+                className="my-4"
+                orientation="vertical"
+                style={{ backgroundColor: theme.primary }}
+              />
+            </div>
+          ))}
         </div>
-        <SidebarTrigger />
-      </SidebarProvider>
+      </div>
       <Separator style={{ backgroundColor: theme.primary }} />
     </header>
   );
